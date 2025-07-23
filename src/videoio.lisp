1b048f7536ed3a283f8ba694a874b17d624d266d
Nico Simoski <nico_simoski@mail.de>
2025-07-23 19:31:29 +0200

    Implemented object-oriented version of video io

--- src/videoio.lisp
+++ src/videoio.lisp
@@ -11,39 +11,49 @@
 (cl-annot:enable-annot-syntax)
 
 @export
-(defun new-camera-video-capture (index)
-  (%new-camera-video-capture index))
+(defclass camera-video-capture (cvo)
+  ((index :accessor camera-index
+          :initarg :index
+          :initform 0
+          :type integer
+          :documentation "Index of the camera to capture the video from.")
+   (image :accessor video-capture-image
+          :initform (make-mat)
+          :documentation "Images are read into existing matrix objects to save memory allocations.")))
 
 @export
-(defun delete-camera-video-capture (capture)
-  (%delete-camera-video-capture capture))
+(defmethod initialize-instance :after ((a-capture camera-video-capture) &key)
+  (unless (cvo-free-func a-capture)
+    (let ((a-pointer (cvo-ptr a-capture)))
+      (trivial-garbage:finalize a-capture
+                                (lambda ()
+                                  (%delete-camera-video-capture a-pointer))))))
 
 @export
-(defun open-camera-video-capture (capture)
-  (%open-camera-video-capture capture))
+(defun make-camera-video-capture (an-index)
+  (make-instance 'camera-video-capture :ptr (%new-camera-video-capture an-index)
+                                       :index an-index))
 
 @export
-(defun video-capture-is-opened? (capture)
-  (%video-capture-is-opened? capture))
+(defmethod open-camera-video-capture ((a-capture camera-video-capture))
+  (%open-camera-video-capture (cvo-ptr a-capture) (camera-index a-capture)))
 
 @export
-(defun read-video-capture-image (capture)
-  (let ((an-image (make-mat)))
-    (%read-video-capture-image capture (cvo-ptr an-image))
+(defmethod video-capture-is-opened? ((a-capture camera-video-capture))
+  (%video-capture-is-opened? (cvo-ptr a-capture)))
+
+@export
+(defmethod read-image ((a-capture camera-video-capture))
+  (let ((an-image (video-capture-image a-capture)))
+    (%read-video-capture-image (cvo-ptr a-capture) (cvo-ptr (video-capture-image a-capture)))
     an-image))
 
 @export
-(defun grab-video-capture-image (capture)
-  (%grab-video-capture-image capture))
+(defmethod grab-image ((a-capture camera-video-capture))
+  (%grab-video-capture-image (cvo-ptr a-capture)))
 
 @export
-(defun retrieve-video-capture-image (capture)
-  (let ((an-image (make-mat)))
-    (%retrieve-video-capture-image capture (cvo-ptr an-image))
+(defmethod retrieve-image ((a-capture camera-video-capture))
+  (let ((an-image (video-capture-image a-capture)))
+    (%retrieve-video-capture-image (cvo-ptr a-capture) (cvo-ptr (video-capture-image a-capture)))
     an-image))
-
-@export
-(defmacro with-camera-video-capture ((a-capture index) &body body)
-  `(let ((,a-capture (new-camera-video-capture ,index)))
-     (unwind-protect (progn ,@body)
-       (delete-camera-video-capture ,a-capture))))
